3d1def47b6d07cd95d8fa3701a8575b09391af78
Florian Heemeyer <florian.heemeyer@gmail.com>
2023-11-13 18:36:36 +0100

    Added comments to ros node

--- scripts/rosurgical_node.py
+++ scripts/rosurgical_node.py
@@ -39,35 +39,38 @@ from rosurgical_lib.utils import parse_topic_parameters
 
 
 if __name__ == '__main__':
-    # Initialize node
+    # Initialize the ROS node
     rospy.init_node('rosurgical_node')
 
-    # TCP role
+    # Get the TCP role from ROS parameters (should be either 'server' or 'client')
     tcp_role = rospy.get_param('~tcp_role')
     assert tcp_role in ['server', 'client'], 'tcp_role should be either "server" or "client"'
 
-    # Socket parameters
+    # Retrieve hostname and port from ROS parameters
     hostname = rospy.get_param('~hostname')
     port = rospy.get_param('~port')
+    
+    # Ensure server's port is above 1024 for non-root binding
     if tcp_role == 'server':
         assert port > 1024, 'The port of the server must be above 1024. If you need a port below 1024, you can set a port above 1024 and redirect the desired incoming connection at the desired port to your port locally.'
 
-    # SSL cerificates
+    # Determine if SSL should be wrapped around the socket and load certificate paths
     wrap_ssl = rospy.get_param('~wrap_ssl', True)
     if wrap_ssl:
         cert_path = os.path.expanduser(rospy.get_param('~cert_path'))
         key_path = os.path.expanduser(rospy.get_param('~key_path'))
         cert_verify_path = os.path.expanduser(rospy.get_param('~cert_verify_path'))
 
+        # Check if SSL certificate files exist
         for path in [cert_path, key_path, cert_verify_path]:
             assert os.path.exists(path), f"File could not be found: {path}"
 
-    # Get topic details
+    # Load topic configuration from a YAML file
     yaml_dir = rospy.get_param('~topic_yaml')
     assert os.path.exists(yaml_dir), f"Topic yaml could not be found {yaml_dir}"
     topic_names, message_types, ros_roles = parse_topic_parameters(tcp_role, yaml_dir)
 
-    # Initialize socket
+    # Initialize the ROSurgical socket (server or client) based on the TCP role
     try:
         if tcp_role == 'server':
             socket = ROSurgicalServer(hostname, port, message_types, topic_names, ros_roles, cert_path, key_path, cert_verify_path)
@@ -76,9 +79,9 @@ if __name__ == '__main__':
     
     except Exception as e:
         rospy.logerr(f"An error occurred: {e}")
-        raise
     
     finally:
+        # Clean up: close the socket when the node is shutting down or an error occurs
         if 'socket' in locals() or 'socket' in globals():
             socket.socket.close()
             rospy.loginfo(f"Closed socket {hostname}:{port}")
